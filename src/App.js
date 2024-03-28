e682dda350817b87b5d9b221bd80ee2ef26cd192
DevanshiShah-19 <devanshishah.tcs2024@gmail.com>
2024-03-28 17:11:05 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import { Container } from "@mui/material";
+import CakeSection from "./components/CakeSection";
+import ContactUs from "./components/ContactUs";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#864313",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+   <Navbar/>
+   <Container>
+        <HeroSection />
+        <CakeSection/>
+        <ContactUs/>
+   </Container> 
+   </ThemeProvider>
   );
 }
 
---
+++ src/components/HeroSection.js
@@ -0,0 +1,24 @@
+import { Box, Button, Grid, Typography } from '@mui/material'
+import React from 'react'
+import Cake from '../images/cake.png'
+
+const HeroSection = () => {
+  return (
+    <Grid container direction="row" alignItems="center">
+      <Grid item xs={12} sm={6} order={{xs:2,sm:1}}>
+        <Typography variant="h3">
+          A bliss in every byte
+        </Typography>
+        <Typography variant="h5">
+          we offer tasty cakes and sweets for you.
+        </Typography>
+        <Button size="large" variant="contained" sx={{borderRadius:5}}>CALL US</Button>
+      </Grid>
+      <Grid container item xs={12} sm={6} justifyContent="center" order={{ xs: 1, sm: 2 }}>
+        <Box component="img" src={Cake}/>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default HeroSection
